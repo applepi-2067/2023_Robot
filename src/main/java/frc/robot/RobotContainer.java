c1bfc571a6ac65425c38a97bf46b429cd5c5b0df
James <jcorcoran@thayermahan.com>
2023-01-22 00:44:32 -0500

    fix compilation errors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,8 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-import robot.subsystems.DriveTrain;
-import robot.Constants.OperatorConstants;
+import frc.robot.subsystems.*;
+import frc.robot.Constants.*;
 
 /**
  * This class is where the bulk of the robot should be declared. 
