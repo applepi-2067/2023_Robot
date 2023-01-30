6dbd00519d0ccead9f7066f98e42c58dc305ed44
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-01-29 19:30:55 -0500

    add trapezoidal vel and accel constraints to rotation

--- src/main/java/frc/robot/commands/drivetrain/RotateToPosition.java
+++ src/main/java/frc/robot/commands/drivetrain/RotateToPosition.java
@@ -3,19 +3,28 @@
 package frc.robot.commands.drivetrain;
 
 import frc.robot.subsystems.Drivetrain;
-import io.github.oblarg.oblog.Loggable;
-import io.github.oblarg.oblog.annotations.Log;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import java.lang.Math;
+
+import io.github.oblarg.oblog.Loggable;
+import io.github.oblarg.oblog.annotations.Log;
 
 public class RotateToPosition extends CommandBase implements Loggable {
     private Drivetrain m_driveTrain;
     private double m_degrees;
-    private double m_acceptableErrorDegrees = 1;
-    private PIDController m_pidController = new PIDController(0.0125, 0, 0);
+    private double m_acceptableErrorDegrees = 0.5;
+
+    // Velocity and acceleration constrained PID control. maxVelocity and maxAcceleration are deg/s and deg/s^2, respectively
+    // Ex. a maxAcceleration of 10 deg/s^2 would reach a 40 deg/s angular velocity in 4 seconds
+    private TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(60, 30);
+    private ProfiledPIDController m_pidController = new ProfiledPIDController(0.01, 0.08, 0, constraints);
+
+
     private boolean wasWithinTolerance = false;
 
     public RotateToPosition(Drivetrain driveTrain, double degrees) {
@@ -29,6 +38,12 @@ public class RotateToPosition extends CommandBase implements Loggable {
     @Override
     public void initialize() {
         m_driveTrain.resetGyro();
+        m_pidController.reset(0);
+
+        // Sets a limit on integrator range, allowing for more aggressive integrator accumulation via high Ki
+        // without runaway accumulation. Set empirically by setting Kp to zero and Ki to a small value, then
+        // increasing maximumIntegral until the max integral term is large enough to move to the set point on its own
+        m_pidController.setIntegratorRange(-0.25, 0.25);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -67,8 +82,10 @@ public class RotateToPosition extends CommandBase implements Loggable {
     private double getRotationPower() {
         double rotationPower = m_pidController.calculate(m_driveTrain.getYaw(), m_degrees);
         rotationPower = MathUtil.clamp(rotationPower, -0.5, 0.5);
+        SmartDashboard.putNumber("Position Error", m_pidController.getPositionError());
+        SmartDashboard.putNumber("Angle Error", getAngleError());
+        SmartDashboard.putNumber("Applied power", rotationPower);
 
-        // System.out.println("Error: " + getAngleError() + ", Power: " + rotationPower + ", Yaw: " + m_driveTrain.getYaw());
         return rotationPower; 
     }
 
