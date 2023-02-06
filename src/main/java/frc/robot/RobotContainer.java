c55d374e171d02e3a3b0a0252b455d7ec6d4fd89
Felix Elston <felixelston07@gmail.com>
2023-02-06 18:47:32 -0500

    fixed code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,13 +86,14 @@ public class RobotContainer {
     //Operator Controls
     //m_operatorContoller.a().onTrue(new SetWaistPosition(0));
     //m_operatorContoller.b().onTrue(new SetWaistPosition(10));
-    m_operatorContoller.leftBumper().onTrue(new SetArmExtension(0));
+    /**m_operatorContoller.leftBumper().onTrue(new SetArmExtension(0));
     m_operatorContoller.rightBumper().onTrue(new SetArmExtension(0.5));
     
     m_operatorContoller.x().onTrue(new SetShoulderPosition(90));
     m_operatorContoller.y().onTrue(new SetShoulderPosition(270));
-    m_operatorContoller.a().onTrue(new DriveShoulderWithJoystick(()->{return 0.0;}));
-  }
+    m_operatorContoller.a().onTrue(new DriveShoulderWithJoystick(()->{return 0.0;})); 
+    */
+  } 
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
