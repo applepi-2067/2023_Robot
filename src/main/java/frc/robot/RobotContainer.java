c03bb7a6a40a9fa1ad1c8c409eec406a39c0ae1d
CJc318 <cjmills918@gmail.com>
2023-02-09 20:02:27 -0500

    added instance for claw pnumatics

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -44,14 +46,16 @@ public class RobotContainer implements Loggable{
   private final Shoulder m_shoulder = Shoulder.getInstance();
   private final Vision m_vision = Vision.getInstance();
   private final Arm m_arm = Arm.getInstance();
+  private final ClawGrasp m_ClawGrasp = ClawGrasp.getInstance();
   private static DigitalInput m_practiceBotJumper = new DigitalInput(Constants.DiscreteInputs.PBOT_JUMPER_DI);
-
+  private Compressor m_compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+  
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
     Logger.configureLoggingAndConfig(this, false);
-
+    m_compressor.enableDigital();
     // Configure the trigger bindings.
     configureBindings();
 
