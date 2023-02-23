85644f88f5b42c3d496b856323b9502292914f65
Amos Cao <amoscao@umich.edu>
2023-02-23 18:02:36 -0500

    remove unneeded encoder zero

--- src/main/java/frc/robot/commands/arm/ZeroArmPosition.java
+++ src/main/java/frc/robot/commands/arm/ZeroArmPosition.java
@@ -19,9 +19,7 @@ public class ZeroArmPosition extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    m_arm.resetEncoders();
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
