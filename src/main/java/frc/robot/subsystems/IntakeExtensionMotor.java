f662fb282f1931bb11af9858c18c0633ee9e8a3e
James <jcorcoran@thayermahan.com>
2023-02-25 22:24:23 -0500

    remove unused imports

--- src/main/java/frc/robot/subsystems/IntakeExtensionMotor.java
+++ src/main/java/frc/robot/subsystems/IntakeExtensionMotor.java
@@ -7,17 +7,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
-import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.REVPhysicsSim;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import frc.robot.Constants;
 import frc.robot.Constants.CANDeviceIDs;
-import frc.robot.commands.intake.SetIntakeExtension;
 import frc.robot.utils.Util;
 import frc.robot.utils.Gains;
 
