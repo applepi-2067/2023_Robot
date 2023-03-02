2e414812227588174038bda3c846e8f502a37488
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-03-02 18:29:08 -0500

    finished deploy preloaded

--- src/main/java/frc/robot/commands/auto/DeployPreloadedPiece.java
+++ src/main/java/frc/robot/commands/auto/DeployPreloadedPiece.java
@@ -4,7 +4,15 @@
 
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.arm.SetArmExtension;
+import frc.robot.commands.arm.ZeroArmPosition;
+import frc.robot.commands.claw.ClawClose;
+import frc.robot.commands.claw.ClawOpen;
+import frc.robot.commands.shoulder.SetShoulderPosition;
+import frc.robot.commands.shoulder.ZeroShoulderPosition;
 import frc.robot.commands.waist.SetWaistPosition;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -16,6 +24,17 @@ public class DeployPreloadedPiece extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
+      new ClawClose(),
+      Commands.parallel(
+        new ZeroShoulderPosition(),
+        new ZeroArmPosition().andThen(new SetArmExtension(0.0))
+      ),
+      new SetShoulderPosition(17.0),
+      new SetArmExtension(0.81),
+      new SetShoulderPosition(5.0),
+      new ClawOpen(),
+      new SetArmExtension(0.0),
+      new SetShoulderPosition(Constants.ScoringPositions.STOWED_POSITION_SHOULDER_DEGREES)
     );
   }
 }
