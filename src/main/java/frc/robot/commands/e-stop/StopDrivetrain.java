aa19a0348da196f04d752a7b9be221e0cd1f0424
Amos Cao <amoscao@umich.edu>
2023-03-03 15:41:10 -0500

    add drivetrain e-stop command

---
+++ src/main/java/frc/robot/commands/e-stop/StopDrivetrain.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.drivetrain;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import java.lang.Math;
+
+/** Make the drivetrain top by setting wheel velocity setpoint to zero
+*/
+public class StopDrivetrain extends CommandBase {
+    private static Drivetrain m_drivetrain;
+
+    public StopDrivetrain(double meters) { 
+        Drivetrain drivetrain = Drivetrain.getInstance(); 
+        addRequirements(drivetrain);
+        m_drivetrain = drivetrain;
+    }
+
+    @Override
+    public void initialize() {}
+
+
+    @Override
+    public void execute() {
+        m_drivetrain.setSetPointVelocity(0.0, 0.0)
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
