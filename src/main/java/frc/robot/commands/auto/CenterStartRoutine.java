2f04a1d2f7cd24b6fc307d09efbfd1bfe20a5478
Moose <reidcore11@gmail.com>
2023-03-04 14:08:01 -0500

    Fixes numbers (Angles and distances)

--- src/main/java/frc/robot/commands/auto/CenterStartRoutine.java
+++ src/main/java/frc/robot/commands/auto/CenterStartRoutine.java
@@ -5,9 +5,11 @@
 package frc.robot.commands.auto;
 
 import frc.robot.commands.arm.SetArmExtension;
+import frc.robot.commands.arm.ZeroArmPosition;
 import frc.robot.commands.chargestation.DriveForwardUntilAngle;
 import frc.robot.commands.chargestation.balanceOnCharge;
 import frc.robot.commands.shoulder.SetShoulderPosition;
+import frc.robot.commands.shoulder.ZeroShoulderPosition;
 import frc.robot.commands.waist.SetWaistPosition;
 import frc.robot.commands.claw.ClawClose;
 import frc.robot.commands.claw.ClawOpen;
@@ -16,16 +18,15 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class CenterStartRoutine extends SequentialCommandGroup {
-  /** Creates a new TwoBall. */
   public CenterStartRoutine() {
     addCommands(
-        new SetShoulderPosition(130.0),
-        new SetArmExtension(1.2192), // 48 inches in meters
+        new ZeroAll(),
+        new SetShoulderPosition(160.0),  // Forward is 20 deg  -- High scoring position
+        new SetArmExtension(0.82), // High scoring position
         new ClawOpen(),
         new SetArmExtension(0.0),
         new ClawClose(),
-        new SetWaistPosition(0),
-        new SetShoulderPosition(25.0),
+        new SetShoulderPosition(-65.0), // down in front
         new DriveForwardUntilAngle(),
         new balanceOnCharge()
     // Gyro to test if level
