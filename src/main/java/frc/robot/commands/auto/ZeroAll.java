46602c2c9d471070e4bb414596445a30c4c7bd78
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-03-05 20:50:28 -0500

    remove unused imports

--- src/main/java/frc/robot/commands/auto/ZeroAll.java
+++ src/main/java/frc/robot/commands/auto/ZeroAll.java
@@ -3,16 +3,11 @@ package frc.robot.commands.auto;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants;
 import frc.robot.commands.arm.SetArmExtension;
 import frc.robot.commands.arm.ZeroArmPosition;
-import frc.robot.commands.intake.ZeroTopLeftIntake;
-import frc.robot.commands.intake.ZeroTopRightIntake;
-import frc.robot.commands.shoulder.SetShoulderPosition;
 import frc.robot.commands.shoulder.ZeroShoulderPosition;
 import frc.robot.commands.waist.SetWaistPosition;
 import frc.robot.commands.waist.ZeroWaistPosition;
-import frc.robot.commands.waist.ZeroWaistPositionCoarse;
 
 public class ZeroAll extends SequentialCommandGroup {
     public ZeroAll() {
@@ -23,7 +18,6 @@ public class ZeroAll extends SequentialCommandGroup {
             ),
             Commands.parallel(
                 new ZeroWaistPosition().andThen(new WaitCommand(0.5)).andThen(new SetWaistPosition(0))
-                // new ZeroTopLeftIntake().andThen(new ZeroTopRightIntake())  // disabled because intake is taken apart
             )
         );
     }
