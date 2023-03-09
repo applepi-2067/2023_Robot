07c6151306b2d1a8149b9bfefe078b03d4a4b20b
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-03-09 18:43:28 -0500

    tweak pickup angle

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -138,7 +138,7 @@ public class RobotContainer implements Loggable {
     m_operatorController.x().onTrue(new SetShoulderPosition(22).andThen(new SetArmExtension(0.894))); // High cone scoring position
     m_operatorController.povDown().onTrue(new SetShoulderPosition(15).andThen(new SetArmExtension(0.894))); // High cube scoring position
     m_operatorController.b().onTrue(new SetShoulderPosition(10).andThen(new SetArmExtension(0.429))); // Mid scoring position
-    m_operatorController.y().onTrue(new SetShoulderPosition(10).andThen(new SetArmExtension(0.18)));  //Get Game Piece from human / feed station
+    m_operatorController.y().onTrue(new SetShoulderPosition(9).andThen(new SetArmExtension(0.18)));  //Get Game Piece from human / feed station
     
     m_operatorController.rightBumper().onTrue(new SetArmExtension(0.005).asProxy().andThen(new SetShoulderPosition(-55.0).asProxy()).andThen(new SetWaistPosition(0)));
     m_operatorController.leftBumper().onTrue(new SetArmExtension(0.005).asProxy().andThen(new SetShoulderPosition(-55.0).asProxy()).andThen(new SetWaistPosition(180)));
