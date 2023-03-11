73bc48b5c24341d1d6c3cd6f718ccfc4adc9ca82
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-03-11 10:17:30 -0500

    added run clawbelt only when lights yellow

--- src/main/java/frc/robot/commands/claw/SetClawBeltSpeed.java
+++ src/main/java/frc/robot/commands/claw/SetClawBeltSpeed.java
@@ -4,13 +4,17 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClawBelt;
+import frc.robot.subsystems.Lights;
+import frc.robot.subsystems.Lights.Color;
 
 public class SetClawBeltSpeed extends CommandBase {
     private ClawBelt m_ClawBelt; 
+    private Lights m_lights;
     private DoubleSupplier m_ClawBeltSpeed; 
 
     public SetClawBeltSpeed(DoubleSupplier clawBeltSpeed) {
         m_ClawBelt = ClawBelt.getInstance();
+        m_lights = Lights.getInstance();
         addRequirements(m_ClawBelt);
         
         m_ClawBeltSpeed = clawBeltSpeed;
@@ -21,7 +25,11 @@ public class SetClawBeltSpeed extends CommandBase {
     
     @Override
     public void execute() {
-        m_ClawBelt.setSpeed(m_ClawBeltSpeed.getAsDouble());
+        if (m_lights.getColor() == Lights.Color.YELLOW) {
+            m_ClawBelt.setSpeed(m_ClawBeltSpeed.getAsDouble());
+        } else {
+            m_ClawBelt.setSpeed(0.0);
+        }
     }
   
     @Override
