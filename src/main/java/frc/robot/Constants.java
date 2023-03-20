78825b1e1d9fff003391e5e937ce053645560cff
CJc318 <cjmills918@gmail.com>
2023-03-20 17:49:15 -0400

    changed robot motor acceleration

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -105,7 +105,7 @@ public final class Constants {
     // Drivetrain only moves when abs(stick) > deadband. Compensates for stick drift.
     public static final double DRIVETRAIN_CONTROLLER_DEADBAND = 0.03;
 
-    public static final double MOTOR_ACCELERATION = 5.0;  // m/s^2
+    public static final double MOTOR_ACCELERATION = 5.5;  // m/s^2
     public static final double MOTOR_TURN_ACCELERATION = 7.0;  // m/s^2, speed differential of the wheels
     public static final double MOTOR_ACCELERATION_AUTO = 3.0;  // m/s^2
   }
