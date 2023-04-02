9f9dce7e3d203893d282d5b8c8067cc7536dd9a0
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-04-02 12:48:03 -0400

    remove color conditional claw roller setting

--- src/main/java/frc/robot/commands/claw/SetClawBeltSpeed.java
+++ src/main/java/frc/robot/commands/claw/SetClawBeltSpeed.java
@@ -4,17 +4,13 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClawBelt;
-import frc.robot.subsystems.Lights;
-import frc.robot.subsystems.Lights.Color;
 
 public class SetClawBeltSpeed extends CommandBase {
     private ClawBelt m_ClawBelt; 
-    private Lights m_lights;
     private DoubleSupplier m_ClawBeltSpeed; 
 
     public SetClawBeltSpeed(DoubleSupplier clawBeltSpeed) {
         m_ClawBelt = ClawBelt.getInstance();
-        m_lights = Lights.getInstance();
         addRequirements(m_ClawBelt);
         
         m_ClawBeltSpeed = clawBeltSpeed;
@@ -25,11 +21,7 @@ public class SetClawBeltSpeed extends CommandBase {
     
     @Override
     public void execute() {
-        if (m_lights.getColor() != Lights.Color.PURPLE) {
-            m_ClawBelt.setSpeed(m_ClawBeltSpeed.getAsDouble());
-        } else {
-            m_ClawBelt.setSpeed(0.0);
-        }
+        m_ClawBelt.setSpeed(m_ClawBeltSpeed.getAsDouble());
     }
   
     @Override
