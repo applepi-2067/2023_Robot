3c8b416684ea82362758abf029b4710ffed060d2
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-04-03 18:38:49 -0400

    removed claw sensor buttons and added onto the Y

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -128,8 +128,7 @@ public class RobotContainer implements Loggable {
       new SetClawBeltSpeed(() -> {return 0.0;})).andThen(
       new DisableBlinkLights()).andThen(
       new DisableLights()));
-    m_operatorController.povUp().onTrue(new ClawSensorGrab());
-    m_operatorController.povLeft().onTrue(new ClawGrabCancel());
+
     
     // Arm locations
     m_operatorController.povRight().onTrue(
@@ -139,7 +138,7 @@ public class RobotContainer implements Loggable {
     m_operatorController.x().onTrue(new SetShoulderPosition(17).andThen(new SetArmExtension(0.780))); // High cone scoring position
     m_operatorController.povDown().onTrue(new SetShoulderPosition(10).andThen(new SetArmExtension(0.894))); // High cube scoring position
     m_operatorController.b().onTrue(new SetShoulderPosition(5).andThen(new SetArmExtension(0.429))); // Mid scoring position
-    m_operatorController.y().onTrue(new SetShoulderPosition(6).andThen(new SetArmExtension(0.18)));  //Get Game Piece from human / feed station
+    m_operatorController.y().onTrue(new SetShoulderPosition(6).andThen(new SetArmExtension(0.18)).alongWith(new ClawSensorGrab()));  //Get Game Piece from human / feed station
    
     m_operatorController.rightBumper().onTrue(new SetArmExtension(0.005).asProxy().andThen(new SetShoulderPosition(Constants.Poses.SHOULDER_STOW_ANGLE).asProxy()).andThen(new SetWaistPosition(0)));
     m_operatorController.leftBumper().onTrue(new SetArmExtension(0.005).asProxy().andThen(new SetShoulderPosition(Constants.Poses.SHOULDER_STOW_ANGLE).asProxy()).andThen(new SetWaistPosition(180)));
