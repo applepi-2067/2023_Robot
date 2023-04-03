8c519f991f439926fe193c0d63ecd4807d7ac533
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-04-03 18:53:11 -0400

    changed mid scoring hight

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -137,7 +137,7 @@ public class RobotContainer implements Loggable {
         new BlockUntilArmLessThan(0.2).andThen(new SetShoulderPosition(Constants.Poses.SHOULDER_STOW_ANGLE)))); // stowed/retracted position
     m_operatorController.x().onTrue(new SetShoulderPosition(17).andThen(new SetArmExtension(0.780))); // High cone scoring position
     m_operatorController.povDown().onTrue(new SetShoulderPosition(10).andThen(new SetArmExtension(0.894))); // High cube scoring position
-    m_operatorController.b().onTrue(new SetShoulderPosition(5).andThen(new SetArmExtension(0.429))); // Mid scoring position
+    m_operatorController.b().onTrue(new SetShoulderPosition(5).andThen(new SetArmExtension(0.391))); // Mid scoring position
     m_operatorController.y().onTrue(new SetShoulderPosition(6).andThen(new SetArmExtension(0.18)).alongWith(new ClawSensorGrab()));  //Get Game Piece from human / feed station
    
     m_operatorController.rightBumper().onTrue(new SetArmExtension(0.005).asProxy().andThen(new SetShoulderPosition(Constants.Poses.SHOULDER_STOW_ANGLE).asProxy()).andThen(new SetWaistPosition(0)));
