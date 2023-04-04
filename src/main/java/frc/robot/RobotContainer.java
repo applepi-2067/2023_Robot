20942c2dc03da6ea6a307651c784033b82ea1e48
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-04-04 19:59:49 -0400

    adjust shoulder angles

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -135,7 +135,7 @@ public class RobotContainer implements Loggable {
       Commands.parallel(
         new SetArmExtension(0.0),
         new BlockUntilArmLessThan(0.2).andThen(new SetShoulderPosition(Constants.Poses.SHOULDER_STOW_ANGLE)))); // stowed/retracted position
-    m_operatorController.x().onTrue(new SetShoulderPosition(17).andThen(new SetArmExtension(0.780))); // High cone scoring position
+    m_operatorController.x().onTrue(new SetShoulderPosition(20).andThen(new SetArmExtension(0.780))); // High cone scoring position
     m_operatorController.povDown().onTrue(new SetShoulderPosition(8).andThen(new SetArmExtension(0.780))); // High cube scoring position
     m_operatorController.b().onTrue(new SetShoulderPosition(5).andThen(new SetArmExtension(0.391))); // Mid scoring position
     m_operatorController.y().onTrue(new SetShoulderPosition(6).andThen(new SetArmExtension(0.0)).alongWith(new ClawSensorGrab()));  //Get Game Piece from human / feed station
