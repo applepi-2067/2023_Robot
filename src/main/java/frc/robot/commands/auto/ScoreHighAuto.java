f286d6b0817815c759e8d7e86862cfc2eb68f2cf
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-04-21 11:10:05 -0400

    change score high to sequential

--- src/main/java/frc/robot/commands/auto/ScoreHighAuto.java
+++ src/main/java/frc/robot/commands/auto/ScoreHighAuto.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.arm.SetArmExtension;
 import frc.robot.commands.shoulder.BlockUntilShoulderGreaterThan;
 import frc.robot.commands.shoulder.SetShoulderPosition;
@@ -12,13 +13,12 @@ import frc.robot.commands.shoulder.SetShoulderPosition;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ScoreHighAuto extends ParallelCommandGroup {
+public class ScoreHighAuto extends SequentialCommandGroup {
   /** Creates a new ScoreHighAuto. */
   public ScoreHighAuto() {
     addCommands(
       new SetShoulderPosition(18.0),  // Forward is 20 deg  -- High scoring position
-      new BlockUntilShoulderGreaterThan(0.0).andThen(
-      new SetArmExtension(0.77)) // High scoring position
+      new SetArmExtension(0.77) // High scoring position
     );
   }
 }
