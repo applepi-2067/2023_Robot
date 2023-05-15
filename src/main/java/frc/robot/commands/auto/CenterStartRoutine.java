1c8b8e71a418c51068a3a6297066e2b3ce58a16c
Apple Pi Robotics <applepirobotics4h@gmail.com>
2023-05-15 18:14:08 -0400

    work in progress cube pick up for center auto

--- src/main/java/frc/robot/commands/auto/CenterStartRoutine.java
+++ src/main/java/frc/robot/commands/auto/CenterStartRoutine.java
@@ -41,15 +41,15 @@ public class CenterStartRoutine extends SequentialCommandGroup {
         new SetArmExtension(0.0),
 
         // Wait and then lower shoulder.
-        new BlockUntilDistanceTraveled(2.0).andThen(new SetShoulderPosition(-65.0)),
-        
-        // Drive backwards to get mobility.
-        new DriveVelocityUntilDistance(-0.5, 4.0).alongWith(new SetWaistPosition(180)).andThen(new GroundPickup()) // TODO: failsafe
+        new BlockUntilDistanceTraveled(2.0).andThen(new SetShoulderPosition(-65.0))
       ),
-
+      
+      Commands.parallel(
+        new DriveVelocityUntilDistance(-0.5, 4.0).alongWith(new SetWaistPosition(180)).andThen(new GroundPickup()), // TODO: fails
+      
       // Drive forwards until we get on the charge station.
-      new DriveVelocityUntilDistance(0.6, 1.45),
-    
+      new DriveVelocityUntilDistance(0.6, 1.45)),
+
       Commands.parallel(
         new BalanceOnCharge(), // TODO: failsafe w/ distance
         new ZeroWaistPosition().andThen(new WaitCommand(0.5)).andThen(new SetWaistPosition(0))
